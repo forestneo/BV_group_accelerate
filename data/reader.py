8666c7dc3d8087030d4be86b0f09d428ef9bd67f
forestneo <dr.forestneo@gmail.com>
2019-03-11 15:24:27 +0800

    add data file

--- data/reader.py
+++ data/reader.py
@@ -6,24 +6,18 @@
 
 import numpy as np
 
-def read_medical_age(filename):
-    f = open(filename, 'r')
-    value_list = f.readlines()
+def read_age(filename):
+    f = open(filename, "r")
+    data = f.read().split()
     f.close()
-
-    ret = []
-    for item in value_list[0:-1]:
-        # print(item, type(item))
-        if len(item.split('.')) != 2:
-            continue
-        if item.split('\n')[0] == ' ':
-            continue
-        value = int(float(item.split("\n")[0]))
-        if value < 100:
-            ret.append(value)
-    return np.asarray(ret)
-
+    res = []
+    for d in data:
+        try:
+            res.append(int(float(d)))
+        except:
+            pass
+    return res
 
 if __name__ == "__main__":
-    print(read_medical_age())
+    print(read_age("test.txt"))
     pass
